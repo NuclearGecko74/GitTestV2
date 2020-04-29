2e48ff306284dddfaec4a72fb8d02d2c02aa827a
NuclearGecko74 <39230191+NuclearGecko74@users.noreply.github.com>
2020-04-29 15:09:45 -0500

    inii
    ffrfrf

---
+++ Source/GitTestEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class GitTestEditorTarget : TargetRules
+{
+	public GitTestEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "GitTest" } );
+	}
+}
